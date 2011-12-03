6326bf9fcbfbdb9b01ae0af7d06694baf88def9e
Yutaka HARA <yutaka.hara.gmail.com>
2011-12-04 02:21:42 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackboard::Application.config.secret_token = '804fa23cd9549907415cc05606a5254ee116f77770e445a40067133e0639917f71a4db83311cbc2d734cfb724e713acaf4c1cbd81b58206be1863d563ab2532d'
