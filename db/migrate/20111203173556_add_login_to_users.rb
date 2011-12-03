67fe274e6398a306080597bc5071ded148d90b30
Yutaka HARA <yutaka.hara.gmail.com>
2011-12-04 02:45:10 +0900

    added index to login
    made login accessible

--- db/migrate/20111203173556_add_login_to_users.rb
+++ db/migrate/20111203173556_add_login_to_users.rb
@@ -1,5 +1,6 @@
 class AddLoginToUsers < ActiveRecord::Migration
   def change
     add_column :users, :login, :string
+    add_index :users, :login,  :unique => true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20111203173556) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
